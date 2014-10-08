9ca92955abefd1709e4f6db5792e948f32767727
Wentao Li <wentaol@student.unimelb.edu.au>
2014-10-08 16:38:21 +1100

    database scheme

---
+++ Dump20141007.sql
@@ -0,0 +1,285 @@
+CREATE DATABASE  IF NOT EXISTS `fooddelivery` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `fooddelivery`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: fooddelivery
+-- ------------------------------------------------------
+-- Server version	5.7.4-m14
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customerinfo`
+--
+
+DROP TABLE IF EXISTS `customerinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customerinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `orderid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customerinfo`
+--
+
+LOCK TABLES `customerinfo` WRITE;
+/*!40000 ALTER TABLE `customerinfo` DISABLE KEYS */;
+INSERT INTO `customerinfo` VALUES (1,'wentao',1),(2,'shady',2),(3,'xi',3),(4,'johan',4),(5,'wenxin',5),(6,'wentao',3),(7,'wentao',4);
+/*!40000 ALTER TABLE `customerinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `deliveraddress`
+--
+
+DROP TABLE IF EXISTS `deliveraddress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `deliveraddress` (
+  `id` int(11) NOT NULL,
+  `orderid` int(11) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `deliveraddress`
+--
+
+LOCK TABLES `deliveraddress` WRITE;
+/*!40000 ALTER TABLE `deliveraddress` DISABLE KEYS */;
+INSERT INTO `deliveraddress` VALUES (1,1,'1a'),(2,2,'2a'),(3,3,'3a'),(4,4,'4a'),(5,5,'5a'),(6,3,'1a'),(7,4,'1a');
+/*!40000 ALTER TABLE `deliveraddress` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `catagory` varchar(45) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `description` text,
+  `menuid` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'afdfaf','dafa',33,'fadfadfasdf',2),(2,'e2r2g','fff',22,'adfasdf',3),(3,'4342','fff',33,'efgbb',2),(4,'adfa','132',122,'asdfasdf',4),(6,'Lamen','noodle',12,'Description for Lamen',8),(7,'adfa','drink',12,'asdfasdf',8),(8,'adfa','drink',12,'asdfasdf',8),(9,'FoodDelivery','132',5,'asdfasdf',8),(10,'Green Tea','tea',6,'best tea!',1),(11,'Rice noodle','noodle',12,'noooodle',1);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `restaurantid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+INSERT INTO `menu` VALUES (1,'menu1_1',1),(2,'menu2_1',1),(3,'menu3_2',2),(4,'menu4_3',3);
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menuitem`
+--
+
+DROP TABLE IF EXISTS `menuitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `menuitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menuid` int(11) NOT NULL,
+  `itemid` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menuitem`
+--
+
+LOCK TABLES `menuitem` WRITE;
+/*!40000 ALTER TABLE `menuitem` DISABLE KEYS */;
+INSERT INTO `menuitem` VALUES (1,1,2),(2,1,3),(3,1,8),(4,1,9),(5,1,10),(6,4,9),(7,4,10),(8,1,10),(9,1,10),(10,1,11),(11,1,11),(12,1,11),(13,1,11),(14,1,11),(15,1,10),(16,1,11),(17,1,12),(18,1,12),(19,10,13),(20,8,14),(21,8,15),(22,8,16),(23,8,17),(24,1,18),(25,1,18),(26,1,18),(27,1,19),(28,1,19),(29,1,12),(30,1,13),(31,1,14),(32,1,10),(33,1,11),(34,1,12),(35,1,10),(36,1,11),(37,1,12),(38,1,12);
+/*!40000 ALTER TABLE `menuitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orderitem`
+--
+
+DROP TABLE IF EXISTS `orderitem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orderitem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `orderid` int(11) DEFAULT NULL,
+  `itemid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderitem`
+--
+
+LOCK TABLES `orderitem` WRITE;
+/*!40000 ALTER TABLE `orderitem` DISABLE KEYS */;
+INSERT INTO `orderitem` VALUES (1,1,1),(2,1,2),(3,1,3),(4,1,4),(5,1,5),(6,2,1),(7,2,2),(8,2,3);
+/*!40000 ALTER TABLE `orderitem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paymentmethod`
+--
+
+DROP TABLE IF EXISTS `paymentmethod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paymentmethod` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `orderid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paymentmethod`
+--
+
+LOCK TABLES `paymentmethod` WRITE;
+/*!40000 ALTER TABLE `paymentmethod` DISABLE KEYS */;
+INSERT INTO `paymentmethod` VALUES (1,'card',1),(2,'card',2),(3,'card',3),(4,'card',4);
+/*!40000 ALTER TABLE `paymentmethod` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pendingorder`
+--
+
+DROP TABLE IF EXISTS `pendingorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pendingorder` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `restaurantid` int(11) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pendingorder`
+--
+
+LOCK TABLES `pendingorder` WRITE;
+/*!40000 ALTER TABLE `pendingorder` DISABLE KEYS */;
+INSERT INTO `pendingorder` VALUES (1,1,'new'),(2,1,'new'),(3,1,'new'),(4,1,'new');
+/*!40000 ALTER TABLE `pendingorder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant`
+--
+
+DROP TABLE IF EXISTS `restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `restaurant` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `averageprice` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+LOCK TABLES `restaurant` WRITE;
+/*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
+INSERT INTO `restaurant` VALUES (1,'Chinese Style','china','northmelbourne',30),(2,'rest2','japan','carlton',15),(3,'rest3','italy','CBD',25),(4,'Colorful Yunnan','china','coberg',20),(5,'Noodle Shop','china','boxhill',15),(6,'Noodle Shop','Noodle Shop','CBD',15),(7,'Noodle Shop','Noodle Shop','CBD',15),(8,'Noodle Shop','Noodle Shop','CBD',15),(9,'Noodle Shop','Noodle Shop','CBD',15),(10,'Noodle Shop','Noodle Shop','CBD',15),(11,'Noodle Shop','Noodle Shop','CBD',15),(12,'Noodle Shop','Noodle Shop','CBD',15),(13,'Noodle Shop','Noodle ShopNoodle Shop','CBD',15),(14,'Noodle Shop','Noodle Shop','CBD',15),(15,'Noodle Shop','Noodle Shop','CBD',15),(16,'Noodle Shop','Noodle Shop','CBD',15),(17,'Noodle Shop','Noodle Shop','CBD',1515),(18,'Noodle Shop','Noodle Shop','CBD',15),(19,'Noodle Shop','Noodle Shop','CBD',15);
+/*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `restaurantid` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'wentao','123456','1'),(2,'cg','1','2'),(3,'c','1','3'),(4,'d','1','4'),(5,'e','1','5'),(6,'sb','1','6'),(7,'2','1','0'),(8,'3','1','0'),(9,'LLL','jjj','0'),(10,'LLL','jjj','0'),(11,'123','456','0'),(12,'123','456','0'),(13,'123','456','0'),(14,'132e','radfa','0'),(15,'xi','123456','0');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-10-07 17:29:15
