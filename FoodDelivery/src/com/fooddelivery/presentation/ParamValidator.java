ac9801fbdab2e1a5746953226485bc597b8eca3b
Wentao Li <wentaol@student.unimelb.edu.au>
2014-10-12 03:00:37 +1100

    added Intercepting Validator

---
+++ FoodDelivery/src/com/fooddelivery/presentation/ParamValidator.java
@@ -0,0 +1,5 @@
+package com.fooddelivery.presentation;
+
+public class ParamValidator {
+	
+}
---
+++ FoodDelivery/src/com/fooddelivery/presentation/SQLValidator.java
@@ -0,0 +1,7 @@
+package com.fooddelivery.presentation;
+
+public class SQLValidator {
+	public String filter(String sql){
+		return sql;
+	}
+}
